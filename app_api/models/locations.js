662425288984d9610fb0791cdb9c15f319d47abc
tringuyen <ndtri98@gmail.com>
2022-10-17 01:20:40 +0200

    API design - chapter06

--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -1,25 +1,51 @@
 var mongoose = require('mongoose');
 
-var openingTimeSchema = new mongoose.Schema({
-    days: {type: String, required: true},
-    opening: String,
-    closing: String,
-    closed: {type: Boolean, required: true}
-});
-
 var reviewSchema = new mongoose.Schema({
     author: String,
-    rating: {type: Number, required: true, min: 0, max: 5},
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
     reviewText: String,
-    createdOn: {type: Date, "default": Date.now}
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
+
+var openingTimeSchema = new mongoose.Schema({
+    days: {
+        type: String,
+        required: true
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true
+    }
 });
 
 var locationSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    name: {
+        type: String,
+        required: true
+    },
     address: String,
-    rating: {type: Number, "default": 0, min: 0, max: 5},
+    rating: {
+        type: Number,
+        "default": 0,
+        min: 0,
+        max: 5
+    },
     facilities: [String],
-    coords: {type: [Number], index: '2dsphere'},
+    // Always store coordinates longitude, latitude order.
+    coords: {
+        type: [Number],
+        index: '2dsphere'
+    },
     openingTimes: [openingTimeSchema],
     reviews: [reviewSchema]
 });
