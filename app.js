227084302e71a29cda35b6645d7df9491717fef7
tringuyen <ndtri98@gmail.com>
2022-10-19 18:42:33 +0200

    Update Angular & geolocation API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./app_api/models/db');
+
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 // var users = require('./app_server/routes/users');
